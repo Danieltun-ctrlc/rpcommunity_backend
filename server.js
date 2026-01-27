55554fb3a9101751a6f91f8dbeffbb891d447324
Chewjiayi <chewjiayi.jyc@gmail.com>
2026-01-27 22:13:34 +0800

    Jiayi

--- server.js
+++ server.js
@@ -281,7 +281,7 @@ app.delete("/posts/:id", async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("Server running on port " + port);
 });
