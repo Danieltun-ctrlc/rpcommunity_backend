c64827b7baf66e2d1136bf3330772b9dee07a06f
Myo Thet Tun <danielsbotinfo@gmail.com>
2026-01-31 01:18:35 +0800

    Update server.js

--- server.js
+++ server.js
@@ -127,20 +127,7 @@ app.post("/login", async (req, res) => {
   }
 });
 
-const allowedOrigins = ["http://localhost:3000"];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) return callback(null, true);
-    return callback(new Error("Not allowed by CORS"));
-  },
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: false,
-};
-
-app.use(cors(corsOptions));
 
 //Event (Jiayi)
 // GET all events
